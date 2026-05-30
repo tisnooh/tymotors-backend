3abd6822dcf09a0a2a9dc8464b45122520c20efd
tisnooh <123783863+tisnooh@users.noreply.github.com>
2026-05-30 03:20:54 +0200

    fix: simple mongo connection

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url, tlsAllowInvalidCertificates=True)
+client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
 app = FastAPI(title="TYMotors API", version="1.0.0")
